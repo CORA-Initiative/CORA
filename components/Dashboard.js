cc68d4dedad91525ba7b6966b8d1099d319d412b
Limuelle <lcalamil@up.edu.ph>
2023-03-04 16:16:28 +0800

    Added login (auth) functionality

---
+++ components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <>
+      <div>Dashboard</div>
+      <div>Delete this if there's already a dashboard</div>
+      <button>Logout</button>
+    </>
+  );
+}
