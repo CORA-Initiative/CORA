4a5c5e682503b9f945b0340e2c8d0ec1b3493d31
Hannah Bella C. Arceño <hcarceno@up.edu.ph>
2023-03-12 17:28:59 +0800

    Added UI screens for taking quiz and results.

---
+++ pages/student/QuizResults.js
@@ -0,0 +1,41 @@
+import React from "react";
+import BackButton from "@/components/BackButton";
+
+export default function QuizResults({
+  test_type = "Pre-Test",
+  passage_title = "Liquids Good For You",
+  score = 2,
+  total_quiz_items = 7,
+}) {
+  return (
+    <div className="flex flex-col p-12 md:px-16 pt-8 gap-6">
+      {/* Back button */}
+      <BackButton />
+
+      <div className="flex flex-col gap-24">
+        <div className="flex flex-col text-center gap-2">
+          <h2 className="text-3xl font-bold">{test_type} Quiz Results</h2>
+          <p className="text-lg">{passage_title}</p>
+        </div>
+
+        <div className="text-center text-gray-800">
+          <p className="text-xl">You completed the {test_type} Quiz!</p>
+          <p className="text-lg">You got</p>
+        </div>
+
+        <div className="text-center flex flex-col gap-4">
+          <h1 className="font-bold text-6xl">
+            {score}/{total_quiz_items}
+          </h1>
+          <p>We may need some practice, kid.</p>
+        </div>
+
+        <div className="flex flex-col items-center mt-4">
+          <button className="bg-coraBlue-1 py-4 px-20 text-coraWhite text-xl font-bold rounded-lg hover:bg-cyan-700">
+            Return Home
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
